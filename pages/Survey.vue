abc68010f0e0ce1ec908208665ed0d9d8e4fce90
tosu! <theo.constant42@gmail.com>
2023-02-11 20:29:28 +0100

    confirm email with enter

--- pages/Survey.vue
+++ pages/Survey.vue
@@ -22,6 +22,7 @@ TODO: use api to suggest and autocomplete the adress
         placeholder="Votre Email"
         class="email"
         :pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/"
+        @keydown.native="startSurvey"
       />
       <Button v-if="!isStarted"
         id="start-survey"
@@ -74,6 +75,11 @@ export default {
       return true;
     },
     startSurvey: function(event) {
+      if (event.type === "keydown") {
+        if (event.key !== "Enter") return;
+        event.preventDefault();
+      }
+
       if (this.isEmailValid()) {
         const emailInput = document.getElementById("email");
         emailInput.disabled = true;
