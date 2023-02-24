dd1804aa5e8cab85af1152098e27ae11e26df557
tosu! <theo.constant42@gmail.com>
2023-02-24 13:02:05 +0100

    info modal

--- components/Utils/InfoCard.vue
+++ components/Utils/InfoCard.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="card">
     <span class="info"
-          @click="revealInfo"
+          @click="openInfoModal"
           v-if="hasInfo()"
     >
       <IconInfo class="info-icon"/>
     </span>
+
     <slot />
+
+    <div class="modal" v-if="isModalOpen">
+      <div class="modal-content">
+              <span class="close-modal" @click="closeInfoModal">
+        <IconClose />
+      </span>
+        <p>INFO MODAL</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,12 +26,20 @@ export default {
   props: {
     infos: Object,
   },
+  data() {
+    return {
+      isModalOpen: false,
+    }
+  },
   methods: {
     hasInfo: function() {
       return this.infos ? Object.keys(this.infos).length > 0 : false;
     },
-    revealInfo: function() {
-      console.log(this.infos);
+    openInfoModal: function() {
+      this.isModalOpen = true;
+    },
+    closeInfoModal: function() {
+      this.isModalOpen = false;
     },
   },
 }
@@ -41,5 +59,34 @@ export default {
   width: 16px;
   height: auto;
   fill: black;
+}
+
+.modal {
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.modal-content {
+  background-color: var(--white);
+  margin: auto;
+  padding: 20px;
+  border-radius: 20px;
+  width: 60%;
+  color: black;
+}
+.close-modal {
+  float: right;
+}
+.close-modal:hover {
+  cursor: pointer;
 }
 </style>
