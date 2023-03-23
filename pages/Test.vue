4a17c1879fb5b068978d08d402ff55b0673dc715
Mael <mael.chanon@etu.u-paris.fr>
2023-03-23 21:08:46 +0100

    test for result

---
+++ pages/Test.vue
@@ -0,0 +1,16 @@
+<template>
+  <ResultMap />
+</template>
+
+<script>
+import ResultMap from "~/components/Utils/ResultMap.Vue";
+
+export default {
+  name: "Test.vue",
+  components: {ResultMap}
+}
+</script>
+
+<style scoped>
+
+</style>
