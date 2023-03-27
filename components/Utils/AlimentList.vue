3a804a7a966a6c7b02999a78ef8da763d45f8a75
Mael <mael.chanon@etu.u-paris.fr>
2023-03-27 19:36:33 +0200

    added result map

---
+++ components/Utils/AlimentList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="AlimentList">
+    dzadzadzddazdzaza
+    <ul>
+      <li v-for="(result,index) in aliments" :key="index">
+        {{ result.nom_aliment }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Aliment",
+  props: {
+    aliments: {
+      type: Array,
+      required: true
+    },
+  },
+}
+
+</script>
+
+<style scoped>
+
+</style>
