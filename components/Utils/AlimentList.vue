df034b9144896d19b4276d7d3f0fc13224b4254b
Mael <mael.chanon@etu.u-paris.fr>
2023-03-29 23:03:35 +0200

    added svg and started result displaying

--- components/Utils/AlimentList.vue
+++ components/Utils/AlimentList.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="AlimentList">
-    dzadzadzddazdzaza
+    <h1 >Liste des aliments les plus consommés pour le {{ this.id }}</h1>
     <ul>
       <li v-for="(result,index) in aliments" :key="index">
-        {{ result.nom_aliment }}
+        {{ result.nom_aliment }} ({{ result.nbChoisi }})
       </li>
     </ul>
   </div>
@@ -17,11 +17,54 @@ export default {
       type: Array,
       required: true
     },
+    id: []
   },
 }
 
 </script>
 
 <style scoped>
+.AlimentList{
+  background-color: #C8F24F;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 30vh;
+}
+.AlimentList h1{
+  height: 5vh;
+}
+ul{
+  margin: 0;
+  padding: 0;
+  display: block;
+  text-align: center;
+  justify-content: center;
+  align-content: center;
+  flex-direction: column;
+  overflow-x: hidden;
+  height: 25vh;
+  overflow-y: auto;
 
+}
+li {
+  list-style-type: none;
+  width: 100%;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  height: 15%;
+  align-items: center;
+  /*padding: 0px 10px 0px 10px;*/
+  color: #ffffff;
+}
+
+
+li:nth-child(even){
+  background-color: var(--dark-blue);
+}
+li:nth-child(odd){
+  background-color: var(--light-blue);
+}
 </style>
--- components/Utils/searchResult.vue
+++ components/Utils/searchResult.vue
@@ -29,4 +29,5 @@ export default {
 ul{
   color:black;
 }
+
 </style>
