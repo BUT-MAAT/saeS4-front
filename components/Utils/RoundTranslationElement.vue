b711a8dd65120d671284f73158111cf3cbd08de6
Mael <mael.chanon@etu.u-paris.fr>
2023-05-04 21:17:40 +0200

    started result page

---
+++ components/Utils/RoundTranslationElement.vue
@@ -0,0 +1,95 @@
+<template>
+  <div ref='card' @mousemove="handleHover" @mouseleave="resetStyle" :style="{ backgroundColor: backColor }" class="round text flex-align-column">
+      <div :style="{ color: textColor }" class="text-content normalText flex-align-column">{{text[0]}}</div>
+      <div :style="{ color: highlightTextColor }" class="text-content highlight flex-align-column">{{highltightedText}}</div>
+      <div :style="{ color: textColor }" class="text-content normalText flex-align-column">{{text[1]}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Input",
+  props: {
+    label: String,
+    backColor: {
+      type: String,
+      default: "#ff0000", //TODO regex for back-color
+    },
+    textColor: {
+      type: String,
+      default: "#ffffff", //TODO regex for back-color
+    },
+    highlightTextColor: {
+      type: String,
+      default: "#004DFFFF", //TODO regex for back-color
+    },
+    text:{
+      type:Array, //TODO voir si on peut limiter la longueur
+    },
+    highltightedText:{
+      type:String,
+    }
+  },
+  data(){
+    const TRESHOLD  = 23;
+    return{
+      THRESHOLD: TRESHOLD,
+    }
+  },
+  methods:{
+    handleHover: function (e){
+      this.$refs.card.classList.remove("soft-transition")
+      const { clientX, clientY, currentTarget } = e;
+      const { clientWidth, clientHeight, offsetLeft, offsetTop } = currentTarget;
+      const horizontal = (clientX - offsetLeft) / clientWidth;
+      const vertical = (clientY - offsetTop) / clientHeight;
+
+      const rotateX = (this.THRESHOLD / 2 - horizontal * this.THRESHOLD).toFixed(2);
+      const rotateY = (vertical * this.THRESHOLD - this.THRESHOLD / 2).toFixed(2);
+      this.$refs.card.style.transform =
+        `perspective(${clientWidth}px) rotateX(${rotateY}deg) rotateY(${rotateX}deg) scale3d(1, 1, 1)`;
+      },
+    resetStyle: function(e){
+      this.$refs.card.classList.add("soft-transition")
+      this.$refs.card.style.transform =
+        `perspective(${e.currentTarget.clientWidth}px) rotateX(0deg) rotateY(0deg)`;
+
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+.round{
+  width: 40vw;
+  height: 40vw;
+  border-radius: 50%;
+  transform-style: preserve-3d;
+
+}
+
+
+.soft-transition{
+  transition: transform 1s ease;
+}
+ .text-content {
+   font-family: "Century Gothic", CenturyGothic, Geneva, AppleGothic, sans-serif;
+  width: 80%;
+  font-size: calc(1vw + 1vh);
+  text-align: center;
+  height: auto;
+}
+.text .highlight{
+  font-size: calc(1.8vw + 1.8vh);
+  font-weight: bold;
+}
+.flex-align-column{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+}
+
+</style>
--- pages/Test.vue
+++ pages/Test.vue
@@ -1,16 +1,45 @@
 <template>
-  <ResultMap />
+  <div>
+    <round-translation-element v-for="(value,key) in textAdhesion"
+      :highltightedText=infoData[key]
+      :text=value
+    />
+    <ResultMap
+    />
+  </div>
+
+
 </template>
 
 <script>
 import ResultMap from "~/components/Utils/ResultMap.Vue";
+import RoundTranslationElement from "../components/Utils/RoundTranslationElement.vue";
 
 export default {
   name: "Test.vue",
-  components: {ResultMap}
+  components: {RoundTranslationElement, ResultMap},
+  mounted() {
+    const url = "http://localhost:9000/api/statistiques/"
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) =>{
+        this.infoData[0] = data.nombre_reponses
+        console.log(this.infoData)
+      })
+  },
+  data(){
+    return{
+      textAdhesion:[["jusqu'à aujourd'hui, le formulaire a été remplis "," fois"]],
+
+      infoData:["?","?","?"]
+    }
+  },
+
 }
 </script>
 
 <style scoped>
+div{
 
+}
 </style>
